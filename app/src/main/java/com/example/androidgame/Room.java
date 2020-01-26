a43ef2e55488edd3edfcfa068839c37b51c4dda4
dillo <Dillon.Belton@gmail.com>
2020-01-26 00:30:22 -0600

    Changed a lot of the logic behind map-building.
    A map is now a list of rooms, rather than one constantly growing array.

--- app/src/main/java/com/example/androidgame/Room.java
+++ app/src/main/java/com/example/androidgame/Room.java
@@ -9,10 +9,40 @@ import java.util.List;
 
 import static com.example.androidgame.GameGrid.TILE_HEIGHT;
 import static com.example.androidgame.GameGrid.TILE_WIDTH;
+import static com.example.androidgame.GameGrid.homeRoom;
+import static java.lang.Math.abs;
 
 public class Room {
     Tile[] grid;
+    int centerX;
+    int centerY;
 
+    public void updateCenter(){
+        int top;
+        int bottom;
+        int left;
+        int right;
+
+        left = grid[0].xpos;
+        right = grid[0].xpos;
+        top = grid[0].ypos;
+        bottom = grid[0].ypos;
+
+        for (Tile tile : grid){
+            if (tile.xpos > right)
+                right = tile.xpos;
+            if(tile.xpos < left)
+                left = tile.xpos;
+            if (tile.ypos > bottom)
+                bottom = tile.ypos;
+            if (tile.ypos < top)
+                top = tile.ypos;
+
+
+        }
+        centerX = (left + right)/2;
+        centerY = (top + bottom)/2;
+    }
     public Room(Display display){
 
         int height = 5;
@@ -32,32 +62,40 @@ public class Room {
                 iterator ++;
             }
         }
+        updateCenter();
+        grid[5] .doorway = true;
         grid[24].doorway = true;
     }
 
 //This will need changing to accomadate the different shapes and configurations
     // but its a start?
     public void explore(Room room, Tile offsetTile){
+        GameGrid.Map.add(room);//Map.add(room)
+        updateCenter();
+        offsetTile.doorway = false;
+        int offsetX = (centerX - offsetTile.xpos);
+        int offsetY = (centerY - offsetTile.ypos);
         List<Tile> currentMap = new ArrayList<Tile>();
         int iterator = 0;
-
         for(Tile tile : grid){
             tile.index = iterator;
-            tile.doorway = false;
+            //tile.doorway = false;
             currentMap.add(tile);
             iterator++;
         }
 
         for (Tile tile : room.grid){
             //tile.xpos += TILE_WIDTH;
-            tile.ypos += (offsetTile.ypos-TILE_HEIGHT+ 30);
+            tile.ypos += (offsetTile.ypos+offsetY + TILE_HEIGHT);//offsetTile.ypos ;//+ TILE_HEIGHT;
+            //tile.xpos -= offsetX*2;//+ offsetTile.xpos ;//+ TILE_WIDTH;
         tile.index = iterator;
         currentMap.add(tile);
         iterator++;
     }
 
-        grid = new Tile[currentMap.size()];
-        grid = currentMap.toArray(grid);
+        //grid = new Tile[currentMap.size()];
+        //grid = currentMap.toArray(grid);
+        //updateCenter();
        // grid = (Tile[])currentMap.toArray();
 
     }
